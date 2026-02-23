b2836912b2bce5e22cf66bbbc021b852e2fd6324
MHueby <mhuebschmann@student.cccs.edu>
2026-02-23 12:05:19 -0700

    Add files via upload
    2/16 and 2/18 changes

--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        Restaurant mcDonalds = new Restaurant(30, 2.0, 6.3, 2.0, "5:00am - 12:00am");
-        Restaurant chipotle = new Restaurant(30,3.0, 14.8, 5, "9:00am - 10:00pm");
+        // Instantiate an ArrayList to hold Restaurant Objects
+        ArrayList<Restaurant> restaurantList = new ArrayList<>();
 
-        System.out.println(chipotle.waitTime);
-        System.out.println(chipotle.rating);
-        System.out.println(chipotle.avgPrice);
-        System.out.println(chipotle.distance);
-        System.out.println(chipotle.hoursOpen);
+
+        // Take user input using JOption pane and store in variables
+        String name = JOptionPane.showInputDialog("Enter the name of the restaurant: ");
+        int waitTime = Integer.parseInt(JOptionPane.showInputDialog("Enter the wait time in minutes of the restaurant as a whole number (example 15): "));
+        double rating = Double.parseDouble(JOptionPane.showInputDialog("Enter the rating of the restaurant with a decimal between 0.0 and 5.0 (example 4.2): "));
+        double avgPrice = Double.parseDouble(JOptionPane.showInputDialog("Enter the average price of the restaurant in dollars with a decimal (example 12.50): "));
+        double distance = Double.parseDouble(JOptionPane.showInputDialog("Enter the distance of the restaurant in miles with a decimal (example 6.7): "));
+        String hoursOpen = JOptionPane.showInputDialog("Enter the hours the restaurant is open (example 09:00am - 08:00pm): ");
+
+        // Instantiate restaurant object with those variables
+        Restaurant userRestaurant = new Restaurant(name, waitTime, rating, avgPrice, distance, hoursOpen);
+        System.out.println(userRestaurant);
+
+        // Add the User's Restaurant Object to the array list
+        restaurantList.add(userRestaurant);
+        }
     }
-}
